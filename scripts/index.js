f44cfd8f323cddbea71bab380449db7d39f51c4d
IronPavlo <povilas.kasko@proton.me>
2024-04-02 09:21:34 +0300

    feature bttns, before swiper

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,22 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
-import "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css";
+
+let featuresBttns = document.getElementsByClassName("features-button");
+console.log(featuresBttns);
+let allSpans = Array.from(document.getElementsByTagName("span"));
+featuresBttns[0].addEventListener("mousedown", activateFeatureBttn);
+featuresBttns[1].addEventListener("mousedown", activateFeatureBttn);
+featuresBttns[2].addEventListener("mousedown", activateFeatureBttn);
+async function activateFeatureBttn(element) {
+  for (let i = 0; i < featuresBttns.length; i++) {
+    featuresBttns[i].classList.remove("features-button-active");
+  }
+  if (allSpans.includes(element.srcElement)) {
+    element.srcElement.parentElement.classList.add("features-button-active");
+  }
+  if (!allSpans.includes(element.srcElement)) {
+    element.srcElement.classList.add("features-button-active");
+  }
+}
+
+let burger = document.getElementById("burger");
+//burger.addEventListener("mousedown", );
