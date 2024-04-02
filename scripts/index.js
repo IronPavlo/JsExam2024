17c216816a61157be2d2603e4b4ca4b678a21311
IronPavlo <povilas.kasko@proton.me>
2024-04-02 12:03:00 +0300

    after experiments with swiper& burger menu creation

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
-
 let featuresBttns = document.getElementsByClassName("features-button");
-console.log(featuresBttns);
+
+let features = document.getElementById("features");
+
+let swiperSlidesFeatures = features.getElementsByClassName("swiper-slide");
+
+let swiperFeatures = features.getElementsByClassName("swiper1");
+
 let allSpans = Array.from(document.getElementsByTagName("span"));
+let menu = document.getElementById("menu");
 featuresBttns[0].addEventListener("mousedown", activateFeatureBttn);
 featuresBttns[1].addEventListener("mousedown", activateFeatureBttn);
 featuresBttns[2].addEventListener("mousedown", activateFeatureBttn);
+
 async function activateFeatureBttn(element) {
   for (let i = 0; i < featuresBttns.length; i++) {
     featuresBttns[i].classList.remove("features-button-active");
@@ -16,7 +22,19 @@ async function activateFeatureBttn(element) {
   if (!allSpans.includes(element.srcElement)) {
     element.srcElement.classList.add("features-button-active");
   }
+  //slideFeatureCheck(features, featuresBttns);
 }
-
+async function slideFeatureCheck(swiperWrapper, bttns) {
+  let bttnsArr = Array.from(bttns);
+}
 let burger = document.getElementById("burger");
-//burger.addEventListener("mousedown", );
+burger.addEventListener("click", openMenu);
+async function openMenu(element) {
+  if (menu.style.transform == "translateX(0px)") {
+    menu.style.transform = "translateX(-160px)";
+  } else {
+    menu.style.transform = "translateX(0px)";
+  }
+
+  console.log(menu.style);
+}
